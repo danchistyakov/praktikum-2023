4e355153635b2ff8710f4e4dd01a1229f00a30d6
danchistyakov <@danchistyakov>
2023-10-02 22:12:19 +0300

    й

--- 0/2_control(0).py
+++ 0/2_control(0).py
@@ -1,143 +1,108 @@
+# Импортируем необходимые библиотеки и модули
 from sqlalchemy import create_engine, Column, Integer, String, Boolean, Sequence, text
-# Импортируем необходимые классы и функции из библиотеки SQLAlchemy для работы с базой данных.
-
 from sqlalchemy.orm import sessionmaker, declarative_base
-# Импортируем классы `sessionmaker` и `declarative_base` для создания сессии базы данных и определения моделей.
-
 import openpyxl
-# Импортируем модуль `openpyxl`, который будет использоваться для работы с файлами Excel.
 
+# Создаем базовый класс для определения моделей
 Base = declarative_base()
-# Создаем объект `Base`, который будет использоваться для определения моделей базы данных.
 
+# Определяем модель данных для таблицы в базе данных
 class TupleData(Base):
-    # Определяем класс `TupleData`, который будет соответствовать таблице в базе данных.
-    __tablename__ = 'task_1_control'
-    # Указываем имя таблицы в базе данных, к которой относится этот класс.
+    __tablename__ = 'task_1_control'  # Имя таблицы
+    id = Column(Integer, Sequence('tuple_id_seq'), primary_key=True)  # Поле для идентификации записей
+    int_data = Column(Integer)  # Поле для целых чисел
+    str_data = Column(String(50))  # Поле для строк (максимум 50 символов)
+    bool_data = Column(Boolean)  # Поле для булевых значений
 
-    id = Column(Integer, Sequence('tuple_id_seq'), primary_key=True)
-    # Определяем поле `id` как целое число, автоматически генерирующееся с помощью последовательности.
-    int_data = Column(Integer)
-    # Определяем поле `int_data` как целое число.
-    str_data = Column(String(50))
-    # Определяем поле `str_data` как строку длиной до 50 символов.
-    bool_data = Column(Boolean)
-    # Определяем поле `bool_data` как булево значение (True/False).
-
+# Создаем класс для работы с данными
 class Tuples:
-    # Определяем класс `Tuples`, который будет использоваться для взаимодействия с кортежами.
     def __init__(self, session):
-        # Определяем конструктор класса `Tuples`, который принимает аргумент `session`, представляющий собой сессию базы данных.
         self.session = session
 
+    # Метод для создания кортежа и сохранения его в базе данных
     def create_tuple(self):
-        # Определяем метод `create_tuple`, который создает кортеж, сохраняет его в базе данных и выводит на экран.
-        my_tuple = (1, "Hello", True, 2, "FinUniversity", False, 3, "Python", True, 4)
-        # Создаем кортеж `my_tuple`.
+        input_string = input("Введите элементы кортежа, разделенные пробелом: ")
 
+        # Преобразуем строку в список
+        input_list = input_string.split()
+
+        # Преобразуем список в кортеж
+        result_tuple = tuple(input_list)
+        print("Сформированный кортеж:", result_tuple)
+
+        # Создаем объекты модели и добавляем их в сессию
         self.session.add_all([
-            TupleData(int_data=my_tuple[0], str_data=my_tuple[1], bool_data=my_tuple[2]),
-            TupleData(int_data=my_tuple[3], str_data=my_tuple[4], bool_data=my_tuple[5]),
-            TupleData(int_data=my_tuple[6], str_data=my_tuple[7], bool_data=my_tuple[8]),
-            TupleData(int_data=my_tuple[9]),
+            TupleData(int_data=int(result_tuple[0]), str_data=result_tuple[1], bool_data=bool(result_tuple[2])),
+            TupleData(int_data=int(result_tuple[3]), str_data=result_tuple[4], bool_data=bool(result_tuple[5])),
+            TupleData(int_data=int(result_tuple[6]), str_data=result_tuple[7], bool_data=bool(result_tuple[8])),
+            TupleData(int_data=int(result_tuple[9])),
         ])
-        # Добавляем объекты `TupleData` в сессию базы данных, представляя каждый элемент кортежа как объект.
-        self.session.commit()
-        # Сохраняем изменения в базе данных.
-
+        self.session.commit()  # Сохраняем изменения в базе данных
         print("Кортеж создан и сохранен в MySQL.")
 
+    # Метод для извлечения элементов из базы данных по индексам
     def get_by_index(self):
-        # Определяем метод `get_by_index`, который извлекает элементы из базы данных по индексам и выводит их на экран.
         index1 = int(input("Введите первый индекс: "))
         index2 = int(input("Введите второй индекс: "))
-        # Считываем индексы элементов кортежа с клавиатуры.
 
-        items = self.session.query(TupleData).filter(TupleData.id.in_([index1 + 1, index2 + 1])).all()
-        # Извлекаем элементы кортежа из базы данных по указанным индексам.
+        # Извлекаем данные из базы данных
+        fields_data = self.session.query(TupleData.int_data, TupleData.str_data, TupleData.bool_data).all()
+        flat_list = [item for sublist in fields_data for item in sublist]
 
-        for item in items:
-            print(item.int_data, item.str_data, item.bool_data)
-            # Выводим извлеченные элементы на экран.
+        # Выводим срез данных в заданном диапазоне
+        print(flat_list[index1:index2 + 1])
 
+    # Метод для извлечения среза данных из базы данных по индексам
     def slice_by_index(self):
-        # Определяем метод `slice_by_index`, который извлекает срез элементов кортежа из базы данных и выводит их на экран.
-        items = self.session.query(TupleData).filter(TupleData.id <= 3).all()
-        # Извлекаем элементы кортежа из базы данных с индексами до 3.
+        index1 = int(input("Введите первый индекс: "))
+        index2 = int(input("Введите второй индекс: "))
 
+        # Извлекаем данные из базы данных с использованием фильтрации по диапазону индексов
+        items = self.session.query(TupleData).filter(TupleData.id.between(index1 + 1, index2 + 1)).all()
+
+        # Выводим элементы среза
         for item in items:
             print(item.int_data, item.str_data, item.bool_data)
-            # Выводим извлеченные элементы на экран.
 
+    # Метод для вывода всех элементов из базы данных
     def print_all(self):
-        # Определяем метод `print_all`, который выводит все элементы кортежа из базы данных на экран.
         items = self.session.query(TupleData).all()
-        # Извлекаем все элементы кортежа из базы данных.
 
+        # Выводим все элементы
         for item in items:
             print(item.int_data, item.str_data, item.bool_data)
-            # Выводим извлеченные элементы на экран.
 
+    # Метод для сохранения данных в Excel
     def save_to_excel(self):
-        # Определяем метод `save_to_excel`, который сохраняет данные из базы данных в файл Excel и выводит их на экран.
         items = self.session.query(TupleData).all()
-        # Извлекаем все элементы кортежа из базы данных.
-
         wb = openpyxl.Workbook()
         ws = wb.active
-        # Создаем новый файл Excel и активный лист.
 
+        # Записываем данные в Excel
         for item in items:
             ws.append([item.int_data, item.str_data, item.bool_data])
-            # Добавляем каждый элемент кортежа в Excel-лист как список.
-
-        wb.save("data.xlsx")
-        # Сохраняем файл Excel под названием "data.xlsx".
-
-        for item in items:
             print(item.int_data, item.str_data, item.bool_data)
-            # Выводим элементы кортежа на экран.
 
+        wb.save("data.xlsx")  # Сохраняем файл Excel
         print("Данные сохранены в Excel.")
 
+# Основная функция программы
 def main():
-    # Главная функция программы.
-
     DATABASE_URL = "mysql+pymysql://root:root@localhost:3306"
-    # Указываем URL базы данных MySQL.
-
     engine_no_db = create_engine(DATABASE_URL)
-    # Создаем движок базы данных, но пока не подключаемся к ней.
-
     connection = engine_no_db.connect()
-    # Устанавливаем соединение с MySQL.
-
     connection.execute(text(f"CREATE DATABASE IF NOT EXISTS task_1_control"))
-    # Создаем базу данных "task_1_control", если она не существует.
-
     connection.close()
-    # Закрываем соединение с MySQL.
 
     DATABASE_URL_DB = "mysql+pymysql://root:root@localhost:3306/task_1_control"
-    # Указываем URL базы данных MySQL с именем базы данных.
-
     engine_with_db = create_engine(DATABASE_URL_DB)
-    # Создаем движок базы данных с указанием имени базы данных.
-
     Base.metadata.create_all(engine_with_db)
-    # Создаем таблицы базы данных, определенные в моделях SQLAlchemy.
-
     Session = sessionmaker(bind=engine_with_db)
-    # Создаем сессию базы данных для взаимодействия с ней, связывая ее с двигателем `engine_with_db`.
-
     session = Session()
-    # Создаем объект сессии для выполнения операций с базой данных.
 
     tuples = Tuples(session)
-    # Создаем объект класса `Tuples` и передаем ему сессию базы данных.
 
     while True:
-        # Запускаем бесконечный цикл для работы с меню выбора действий.
         print("""
         1. Создание кортежа, сохранение и вывод из MySQL.
         2. Извлечь элементы по индексам, сохранение и вывод из MySQL.
@@ -146,33 +111,22 @@ def main():
         5. Сохранить данные из MySQL в Excel и вывести на экран.
         6. Выход.
         """)
-
         choice = input("Выберите действие: ")
-        # Запрашиваем у пользователя выбор действия.
-
         if choice == "1":
             tuples.create_tuple()
-            # Вызываем метод для создания кортежа и сохранения его в базе данных.
         elif choice == "2":
             tuples.get_by_index()
-            # Вызываем метод для извлечения элементов по индексам из базы данных.
         elif choice == "3":
             tuples.slice_by_index()
-            # Вызываем метод для извлечения среза элементов по индексам из базы данных.
         elif choice == "4":
             tuples.print_all()
-            # Вызываем метод для вывода всех элементов кортежа из базы данных.
         elif choice == "5":
             tuples.save_to_excel()
-            # Вызываем метод для сохранения данных в Excel и вывода на экран.
         elif choice == "6":
             break
-            # Выходим из цикла при выборе опции "выход".
         else:
             print("Неизвестный выбор!")
-            # Выводим сообщение об ошибке при некорректном выборе действия.
 
+# Запуск основной функции при выполнении скрипта
 if __name__ == "__main__":
     main()
-    # Запускаем главную функцию программы, если скрипт запущен напрямую.
-
