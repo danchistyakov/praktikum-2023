8a1eedff020d6acbfb5e5634a7833cf0d51c0955
Daniil <4i.danila@gmail.com>
2023-11-07 02:23:24 +0300

    Fixes

--- 5_task/5_hard.py
+++ 5_task/5_hard.py
@@ -4,12 +4,24 @@ import random
 import openpyxl
 
 config = {
-  'user': 'root',
-  'password': 'root',
-  'host': '127.0.0.1',
-    'port':'3306',
-  'raise_on_warnings': True
+    'user': 'root',
+    'password': 'root',
+    'host': '127.0.0.1',
+    'port': '3306',
+    'raise_on_warnings': True
 }
+
+
+def set_lists(cursor):
+    raw_list = input('Введите список элементов для задачи 1: ')
+    str_list = raw_list.split(' ')
+    data = list(map(int, str_list))
+    cursor.execute("INSERT INTO task_5_table (data) VALUES ('data')")
+    raw_list = input('Введите список элементов для задачи 1: ')
+    str_list = raw_list.split(' ')
+    data = list(map(int, str_list))
+    cursor.execute("INSERT INTO task_5_table (data) VALUES ('data')")
+
 
 def print_odds_before_stop(cursor):
     stop = 71278
@@ -26,6 +38,7 @@ def print_odds_before_stop(cursor):
     cursor.execute("INSERT INTO task_5_table (data) VALUES (result)")
     return result
 
+
 def remove_number():
     to_remove = 500
     raw_list = input('Введите список элементов: ')
@@ -33,38 +46,43 @@ def remove_number():
     num_list = list(map(int, str_list))
     return [number for number in num_list if number != to_remove]
 
+
 def generate_unique_list():
     rn = [x for x in range(1, 76)]
     random.shuffle(rn)
     return print(rn[:20])
 
+
 def main():
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS task_5")
+    try:
+        cursor.execute("CREATE DATABASE IF NOT EXISTS task_5")
+    except:
+        print("")
     cursor.execute("USE task_5")
 
     create_table_query = """
-    CREATE TABLE IF NOT EXISTS your_table_name (
+    CREATE TABLE IF NOT EXISTS task_5_table (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        column1 VARCHAR(255),
-        column2 INT,
-        column3 DATE
+        data VARCHAR(255)
     );
     """
-    cursor.execute(create_table_query   )
-
+    try:
+        cursor.execute(create_table_query)
+    except:
+        print("")
 
     while True:
         print("""
-        1. Ввод списка, фильтрация по нечётным значениям, сохранение и вывод из MySQL.
-        2. Ввод списка, удаление значения 500, сохранение и вывод из MySQL.
-        3. Генерация списка из случайных значений, сохранение и вывод из MySQL.
+        1. Ввод/генерация списков, сохранение и вывод из MySQL.
+        2. Выполнение всех операций из базового варианта, сохранение результатов и вывод из MySQL (тоже в виде таблицы).
+        3. Сохранить данные из MySQL в Excel и вывести на экран..
         4. Выход.
         """)
         choice = input("Выберите действие: ")
         if choice == "1":
-            print_odds_before_stop(cursor)
+            set_lists(cursor)
         elif choice == "2":
             remove_number()
         elif choice == "3":
