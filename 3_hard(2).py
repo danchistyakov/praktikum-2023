94e6e3db23d20515597655207773adcfea7b0d2f
Beloglazovaaa <Beloglazova2004@mail.ru>
2023-11-09 19:43:27 +0300

    task 3

--- 3_hard(2).py
+++ 3_hard(2).py
@@ -1,12 +1,13 @@
 import mysql.connector
 import openpyxl
 
+# Конфигурация для подключения к MySQL
 config = {
     'user': 'root',
     'password': 'root',
     'host': '127.0.0.1',
     'port': '3306',
-    'database': 'task_3',
+    'database': 'task_3',  # Убедитесь, что база данных с таким именем существует
     'raise_on_warnings': False
 }
 
@@ -60,12 +61,15 @@ def update_dictionary(cursor, list_id):
         values = record[1].split(',')
         dictionary = dict(zip(keys, values))
 
-        new_key = input("Введите ключ для добавления нового элемента в словарь: ")
-        new_value = input("Введите значение для добавления нового элемента в словарь: ")
+        # Добавляем или обновляем элемент в словаре
+        new_key = input("Введите ключ для добавления/обновления: ")
+        new_value = input("Введите значение для добавления/обновления: ")
         dictionary[new_key] = new_value
 
+        # Сохраняем обновленный словарь обратно в MySQL
         save_to_mysql(cursor, list(dictionary.keys()), list(dictionary.values()), list_id)
 
+        # Вывод обновленного словаря, его длины, ключей и значений
         print("Обновленный словарь:", dictionary)
         print("Длина словаря:", len(dictionary))
     else:
@@ -78,23 +82,18 @@ def save_to_excel(cursor, filename="list_dictionary.xlsx"):
 
     wb = openpyxl.Workbook()
     ws = wb.active
-    ws.append(["Список 1", "Список 2", "Обновленный словарь", "Длина словаря"])
+    ws.append(["№", "Словарь", "Длина"])  # Заголовки столбцов
 
     for row in records:
         id, keys_text, values_text = row
         keys = keys_text.split(',')
         values = values_text.split(',')
-        dictionary = dict(zip(keys, values))
-
-        ws.append([
-            keys_text,
-            values_text,
-            str(dictionary),
-            len(dictionary)
-        ])
+        dictionary = dict(zip(keys, values))  # Преобразование в словарь
+        ws.append([id, str(dictionary), len(dictionary)])  # Вывод словаря и его длины
 
     wb.save(filename)
     print(f"Данные успешно сохранены в файл {filename}.")
+
 
 def main():
     connection = connect_to_mysql(config)
@@ -105,9 +104,9 @@ def main():
 
     while True:
         print("\n1. Ввод списков, сохранение и вывод из MySQL.")
-        print("2. Преобразование двух списков в словарь, сохранение и вывод из MySQL.")
-        print("3. Выполнение всех операций из базового варианта, сохранение результатов и вывод из MySQL.")
-        print("4. Сохранение данных из MySQL в Excel и вывод на экран.")
+        print("2. Преобразовать два списка в словарь, сохранение и вывод из MySQL.")
+        print("3. Вывести обновленный словарь и его длину в консоль.")
+        print("4. Сохранить данные из MySQL в Excel и вывести на экран.")
         print("5. Выход.")
         choice = input("Выберите действие: ")
 
@@ -135,4 +134,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
